22d833ec988a037283285f5624154bb736147eb0
notsoseamless <john.oldman@polehill.co.uk>
2017-10-19 10:14:39 +0100

    development

--- calendar_data.py
+++ calendar_data.py
@@ -1,7 +1,7 @@
 '''
 Container with calendar data
 
-John Oldman    October 2017
+notsoseamless -October 2017
 
 '''
 
@@ -20,9 +20,11 @@ class CalData:
         self._birthdays = []
         self._aniversaries = []
         self._appointments = []
+        self._bank_days = []
         self._build_birthdays()
         self._build_aniversaries()
         self._build_appointment()
+        self._build_bank_holidays()
     def _build_birthdays(self):
         '''
         Method:         _build_birthdays
@@ -31,6 +33,7 @@ class CalData:
         Description:    builds the birthdays for a year.
         '''
         self._birthdays.append({'month':'1' , 'day':'14', 'name':'Test'  , 'year':1985})
+        self._birthdays.append({'month':'1' , 'day':'24', 'name':'Test'  , 'year':1900})
     def _build_aniversaries(self):
         '''
         Method:         _build_aniversaries
@@ -39,6 +42,7 @@ class CalData:
         Description:    builds the aniversaries for a year.
         '''
         self._aniversaries.append({'month':'1' , 'day':'27', 'name':'test'            , 'year':2014})
+        self._aniversaries.append({'month':'1' , 'day':'20', 'name':'test'            , 'year':0})
     def _build_appointment(self):
         '''
         Method:         _build_appointment
@@ -46,14 +50,34 @@ class CalData:
         Returns:        None
         Description:    builds the appointments for a year.
         '''
-        self._appointments.append({'month':'1' , 'day':'2' , 'name':'Test'})
-    def get_birthdays(self):
-        ''' getter '''
+        self._appointments.append({'month':'1' , 'day':'2' , 'name':'Test' , 'year':0})
+
+    def _build_bank_holidays(self):
+        '''
+        Method:         _build_bank_holidayss
+        Parameters:     None
+        Returns:        None
+        Description:    builds the bank holidays for a year.
+        '''
+        self._bank_days.append({'month':'1'  , 'day':'1' , 'name':'NEW YEAR'  , 'year':0})
+        self._bank_days.append({'month':'8'  , 'day':'28', 'name':'SUMMER BH' , 'year':0})
+        self._bank_days.append({'month':'12' , 'day':'25', 'name':'XMAS'      , 'year':0})
+        self._bank_days.append({'month':'12' , 'day':'26', 'name':'BOXING DAY', 'year':0})
+      
+
+    @property
+    def birthdays(self):
         return self._birthdays
-    def get_aniversaries(self):
-        ''' getter '''
+    
+    @property
+    def aniversaries(self):
         return self._aniversaries
-    def get_appointments(self):
-        ''' getter '''
+    
+    @property
+    def appointments(self):
         return self._appointments
+    
+    @property
+    def bank_days(self):
+        return self._bank_days
 
